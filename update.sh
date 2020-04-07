72d2b80348317e25bc09dcfbfb507b0ec14bc2d8
vz-shark <vz-shark@miraikanai.net>
2020-04-07 15:29:54 +0900

    plotly change to offline

--- update.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# run daily to update covid stats
-
-jupyter nbconvert --to html --execute curvefit-global.ipynb  --output ./curvefit-global.html
-jupyter nbconvert --to html --execute curvefit-us.ipynb --output ./curvefit-us.html
-
-git add *
-git commit -m 'automatic update'
-git push
