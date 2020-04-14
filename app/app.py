9fab8b14a7071ea1c0a4f4c8aec797e7675681d2
vz-shark <vz-shark@miraikanai.net>
2020-04-14 22:34:01 +0900

    decided app's name to NCS

--- app/app.py
+++ app/app.py
@@ -5,6 +5,7 @@ import sys
 import pickle
 import numpy as np
 import pandas as pd
+import base64
 
 import dash
 import dash_core_components as dcc
@@ -40,54 +41,71 @@ def makefig(df, regionstr, startlim = 10):
 
     return(plot)
 
-#データセット読み込み
-print(f"load dataset")
-jpdata = covdata.CovDataJP()
-jpdata.read()
-jpdata.make_JHU()
-jpdf = jpdata.df_jhu["Comfirmed"]
-
-# 罹患者総数が多い順に並べる
-cases = jpdf.iloc[:,[1,-1]].groupby('Country/Region').sum()
-mostrecentdate = cases.columns[0]
-print('\nTotal number of cases (in countries with at least 100 cases) as of', mostrecentdate)
-cases = cases.sort_values(by = mostrecentdate, ascending = False).drop("クルーズ船")
-#cases = cases[cases[mostrecentdate] >= 100]
-sorted_region = cases.index.values.tolist()
-regions = sorted_region[0:10]
-# for one in ["茨城"]:
-#     if( not one in regions):
-#         regions.append(one)
-print(f"regions: {regions}")
-
-# figを作る
-print("######### make fig #############")
-figpkl = "./cache/cache_fig.pkl"
 plots = {}
-if( os.path.exists(figpkl)):
-    print(f"using cache.....{figpkl}")
-    with open(figpkl, "rb") as fd:
-        plots = pickle.load(fd)
-else:
-    for one in regions:
-        plots[one] = makefig(jpdf, one)
-    print(f"saving cache....{figpkl}")
-    with open(figpkl, "wb") as fd:
-        pickle.dump(plots, fd)
+
+
+enable_fig = True 
+if(enable_fig):
+    print("######### make fig #############")
+    figpkl = "./cache/cache_fig.pkl"
+    if( os.path.exists(figpkl) ):
+        print(f"using cache.....{figpkl}")
+        with open(figpkl, "rb") as fd:
+            plots = pickle.load(fd)
+    else:
+        #データセット読み込み
+        print(f"load dataset")
+        jpdata = covdata.CovDataJP()
+        jpdata.read()
+        jpdata.make_JHU()
+        jpdf = jpdata.df_jhu["Comfirmed"]
+
+        # 罹患者総数が多い順に並べる
+        cases = jpdf.iloc[:,[1,-1]].groupby('Country/Region').sum()
+        mostrecentdate = cases.columns[0]
+        print('\nTotal number of cases (in countries with at least 100 cases) as of', mostrecentdate)
+        cases = cases.sort_values(by = mostrecentdate, ascending = False).drop("クルーズ船")
+        #cases = cases[cases[mostrecentdate] >= 100]
+        sorted_region = cases.index.values.tolist()
+        regions = sorted_region[0:10]
+        # for one in ["茨城"]:
+        #     if( not one in regions):
+        #         regions.append(one)
+        print(f"regions: {regions}")
+
+        for one in regions:
+            plots[one] = makefig(jpdf, one)
+        print(f"saving cache....{figpkl}")
+        with open(figpkl, "wb") as fd:
+            pickle.dump(plots, fd)
 
 
 # htmlコンポーネットを用意
 print("######## make html components ########")
-with open('./elements/description_approximation.md', 'r') as fd:
+with open('./contents/description_approximation.md', 'r') as fd:
     markdown_section_approximation =  fd.read()
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 
+with open("./contents/shark.png", "rb") as fd:
+    b64img = base64.b64encode(fd.read()).decode(encoding='utf-8')
+
+header = html.Header(children=[
+            html.Center( children=[
+                html.Img( src=f"data:image/png;base64,{b64img}", width="4%", height="4%"),
+                html.P("NCS (neko covid system)"),
+                html.Hr()
+            ])
+        ])
+
+print(header)
+
 partsls = []
-partsls += [html.H1(children='Trends of COVID-19 Epidemic In Japan ')]
-partsls += [html.Hr()]
+partsls += [header]
+partsls += [html.H1("Trends of COVID-19 Epidemic in Japan") ]
+partsls += [html.Br() for _ in range(2)]
 partsls += [dcc.Markdown( markdown_section_approximation)]
 for key, val in plots.items():
     partsls += [html.Br()]
